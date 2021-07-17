ae9cf49026000053b70f0bd1e9f33f884d71fbe6
Nguyễn Hoàng <nnhoang215@gmail.com>
2021-07-17 08:33:26 +0700

    My first commit

---
+++ config/config.exs
@@ -0,0 +1,39 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, DiscussWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "KZ3+QKkqV8pbRJqrCoyJw7+4gkSGm8fFIRLskrOU1qcy+T/PniRtzM/Nb8XulyUG",
+  render_errors: [view: DiscussWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Discuss.PubSub,
+  live_view: [signing_salt: "XYBCddvj"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
+
+#Config for ueberauth
+config :ueberauth, Ueberauth,
+  providers: [
+    facebook: { Ueberauth.Strategy.Facebook, [ opt1: "value", opts2: "value" ] },
+    github: { Ueberauth.Strategy.Github, [ opt1: "value", opts2: "value" ] }
+  ]
